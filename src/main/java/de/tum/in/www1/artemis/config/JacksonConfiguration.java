aabd90538e1f1f0bc49c8966eddf2707742b905d
Julian Christl <julian.christl@tum.de>
2023-10-28 23:17:11 +0200

    remove modules

--- src/main/java/de/tum/in/www1/artemis/config/JacksonConfiguration.java
+++ src/main/java/de/tum/in/www1/artemis/config/JacksonConfiguration.java
@@ -2,8 +2,6 @@ package de.tum.in.www1.artemis.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.zalando.problem.jackson.ProblemModule;
-import org.zalando.problem.violations.ConstraintViolationProblemModule;
 
 import com.fasterxml.jackson.datatype.hibernate5.Hibernate5Module;
 import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
@@ -34,21 +32,4 @@ public class JacksonConfiguration {
     public Hibernate5Module hibernate5Module() {
         return new Hibernate5Module();
     }
-
-    /*
-     * Module for serialization/deserialization of RFC7807 Problem.
-     */
-    @Bean
-    public ProblemModule problemModule() {
-        return new ProblemModule();
-    }
-
-    /*
-     * Module for serialization/deserialization of ConstraintViolationProblem.
-     */
-    @Bean
-    public ConstraintViolationProblemModule constraintViolationProblemModule() {
-        return new ConstraintViolationProblemModule();
-    }
-
 }
